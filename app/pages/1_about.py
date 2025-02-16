ed093c714ab301e95293bae9b9702736c9abf5a6
ggaray <ggaray@mrhouston.net>
2025-02-16 12:32:40 +0100

    third

--- app/pages/1_about.py
+++ app/pages/1_about.py
@@ -4,6 +4,8 @@ if "authenticated" not in st.session_state or not st.session_state.authenticated
     st.warning("🔒 Please login from the main page to access this page.")
     st.stop()
 
+st.write("DB username:", st.secrets["db_username"])
+
 st.title("📄 About")
 st.write("This is the About page for the multipage Streamlit app.")
 st.write("Use the sidebar to navigate through different pages.")
